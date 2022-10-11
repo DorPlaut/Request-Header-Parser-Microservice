3823c8496ba644e89f9a331fedfcea6bc2f7b630
dorplaut <dorplaut@gmail.com>
2022-10-12 02:42:48 +0300

    upload

---
+++ index.js
@@ -0,0 +1,51 @@
+// index.js
+// where your node app starts
+
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// my solution
+const { networkInterfaces } = require('os');
+const networkInfo = networkInterfaces();
+
+app.get('/api/whoami', function (req, res) {
+  // get IP adress
+  const ipAdress = []
+    .concat(...Object.values(networkInfo))
+    .filter(
+      (details) => details.family === 'IPv4' && details.internal === false
+    )
+    .pop().address;
+  // get language
+  const localLang = req.headers['accept-language'];
+  // get browser info
+  const browser = req.headers['user-agent'];
+  // end get info
+  // set response
+  res.json({
+    ipaddress: ipAdress,
+    language: localLang,
+    software: browser,
+  });
+});
+
+// listen for requests :)
+// add process.env.PORT || to port
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
